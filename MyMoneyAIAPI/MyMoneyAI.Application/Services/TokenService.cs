7aca1e90a736a9030c4311cd6399fa355f0fb627
Ryan Richard <mrrarichard@gmail.com>
2023-04-01 14:02:19 +0200

    Set up and configured ASP.NET Core Identity. Created a JWT settings class to store the necessary configuration values. Implemented the logic for generating and validating JWT tokens. Added the necessary middleware to the API pipeline for handling authentication. Tested the authentication implementation to ensure it's working as expected.

---
+++ MyMoneyAIAPI/MyMoneyAI.Application/Services/TokenService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MyMoneyAI.Application.Interfaces;
+using MyMoneyAI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoneyAI.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtSettings _jwtSettings;
+
+        public TokenService(IOptions<JwtSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id),
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(_jwtSettings.ExpirationInMinutes),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        public JwtSecurityToken ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+
+            try
+            {
+                tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                return (JwtSecurityToken)validatedToken;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+
+}
+
---
+++ MyMoneyAIAPI/MyMoneyAI.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyMoneyAI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoneyAI.Infrastructure.Data
+{
+    public class ApplicationDbContext: IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
