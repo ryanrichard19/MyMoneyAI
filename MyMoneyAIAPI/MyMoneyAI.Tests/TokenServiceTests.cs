c2e5fd3df2c08593fc372b680deaa75c1b6064ec
Ryan Richard <mrrarichard@gmail.com>
2023-04-01 17:51:27 +0200

    feat: Implement UserRepository with user-related operations
    - Add IUserRepository interface with methods for user-related operations (e.g., RegisterUserAsync, GetUserByIdAsync)
    - Implement UserRepository class using UserManager and other ASP.NET Core Identity components to perform user-related database operations
    - Include error handling in RegisterUserAsync method
    - Add unit tests for TokenService

---
+++ MyMoneyAIAPI/MyMoneyAI.Tests/TokenServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Options;
+using MyMoneyAI.Application.Services;
+using MyMoneyAI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoneyAI.Tests
+{
+    public class TokenServiceTests
+    {
+        [Fact]
+        public void GenerateToken_ShouldReturnValidToken()
+        {
+            // Arrange
+            var jwtSettings = new JwtSettings { Secret = "aVeryLongSecretKeyForTesting", ExpirationInMinutes = 60 };
+            var tokenService = new TokenService(Options.Create(jwtSettings));
+            var user = new User { Id = "1", UserName = "testuser" };
+
+            // Act
+            var token = tokenService.GenerateToken(user);
+
+            // Assert
+            Assert.NotNull(token);
+            var jwtToken = tokenService.ValidateToken(token);
+            Assert.NotNull(jwtToken);
+
+            var idClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "nameid");
+            Assert.NotNull(idClaim);
+            Assert.Equal(user.Id, idClaim.Value);
+
+            var userNameClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "unique_name");
+            Assert.NotNull(userNameClaim);
+            Assert.Equal(user.UserName, userNameClaim.Value);
+        }
+
+        [Fact]
+        public void ValidateToken_ShouldReturnNullForInvalidToken()
+        {
+            // Arrange
+            var jwtSettings = new JwtSettings { Secret = "aVeryLongSecretKeyForTesting", ExpirationInMinutes = 60 };
+            var tokenService = new TokenService(Options.Create(jwtSettings));
+            var invalidToken = "invalid.token.string";
+
+            // Act
+            var jwtToken = tokenService.ValidateToken(invalidToken);
+
+            // Assert
+            Assert.Null(jwtToken);
+        }
+    }
+}
---
+++ MyMoneyAIAPI/MyMoneyAI.Tests/UserRepositoryTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using MyMoneyAI.Domain.Entities;
+using MyMoneyAI.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMoneyAI.Tests
+{
+    public class UserRepositoryTests
+    {
+        [Fact]
+        public async Task RegisterUserAsync_ShouldCreateUser()
+        {
+            // Arrange
+            var userManagerMock = new Mock<UserManager<User>>(Mock.Of<IUserStore<User>>(), null, null, null, null, null, null, null, null);
+            userManagerMock.Setup(um => um.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var userRepository = new UserRepository(userManagerMock.Object);
+            var newUser = new User { UserName = "testuser" };
+            var password = "TestPassword123!";
+
+            // Act
+            var createdUser = await userRepository.RegisterUserAsync(newUser, password);
+
+            // Assert
+            Assert.NotNull(createdUser);
+            Assert.Equal(newUser.UserName, createdUser.UserName);
+            userManagerMock.Verify(um => um.CreateAsync(newUser, password), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetUserByIdAsync_ShouldReturnUser()
+        {
+            // Arrange
+            var userManagerMock = new Mock<UserManager<User>>(Mock.Of<IUserStore<User>>(), null, null, null, null, null, null, null, null);
+            var testUser = new User { Id = "1", UserName = "testuser" };
+            userManagerMock.Setup(um => um.FindByIdAsync(It.IsAny<string>()))
+                .ReturnsAsync(testUser);
+
+            var userRepository = new UserRepository(userManagerMock.Object);
+
+            // Act
+            var user = await userRepository.GetUserByIdAsync(testUser.Id);
+
+            // Assert
+            Assert.NotNull(user);
+            Assert.Equal(testUser.Id, user.Id);
+            Assert.Equal(testUser.UserName, user.UserName);
+            userManagerMock.Verify(um => um.FindByIdAsync(testUser.Id), Times.Once);
+        }
+   
+    }
+}
